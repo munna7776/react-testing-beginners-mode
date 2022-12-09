56e67ec129148292e1f1147e5cda103901aac391
Munna Kumar Barnwal <barnwalmunnakumar@gmail.com>
2022-12-09 15:55:30 +0530

    Application Componenet testing started

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react"
+import Application from "./Application"
+
+describe("Application", ()=>{
+    test("renders correctly", ()=>{
+        render(<Application />)
+        const nameElement = screen.getByRole("textbox")
+        expect(nameElement).toBeInTheDocument()
+
+        const jobLocationElement = screen.getByRole("combobox")
+        expect(jobLocationElement).toBeInTheDocument()
+
+        const termsElement = screen.getByRole("checkbox")
+        expect(termsElement).toBeInTheDocument()
+    })
+})
--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,22 +1,24 @@
-import { render, screen } from "@testing-library/react"
-import Greet from "./Greet"
- /* 
- * Greet should render Hello text and if a name is passed into the component 
+import { render, screen } from "@testing-library/react";
+import Greet from "./Greet";
+/*
+ * Greet should render Hello text and if a name is passed into the component
  * It should render the hello followed by name
  */
 
- /*
+/*
  TDD is a software development process where we write test before writing software code and also making sure that all the test passes.
  */
 
- test("Greet renders correctly", ()=>{
-    render(<Greet />)
-    const textElement = screen.getByText(/hello/i)
-    expect(textElement).toBeInTheDocument()
- })
+describe("Greet", () => {
+  test("Greet renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
- test("Greet renders correctly with a name", ()=>{
-    render(<Greet name="Munna" />)
-    const textElement = screen.getByText(/hello munna/i)
-    expect(textElement).toBeInTheDocument()
- })
+  test("Greet renders correctly with a name", () => {
+    render(<Greet name="Munna" />);
+    const textElement = screen.getByText(/hello munna/i);
+    expect(textElement).toBeInTheDocument();
+  });
+});
